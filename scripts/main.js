97553e4d3c1541897702817a405e3a74e6d89ce7
Denislav Ganchev <denodenodeno@gmail.com>
2015-05-02 18:21:45 +0300

    added reading time info

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,52 @@
 'use strict';
 
-(function () {
+(function (window, document) {
 
-  function share () {
-    var twitter = document.querySelectorAll('.share--tw-js');
-    var position = document.location.href;
-    var title = document.title;
-    var twitterLink = 'https://twitter.com/intent/tweet?url=' + position + '&text=' + title + ' via @denoStc';
+  var app = {
+      init: function () {
+          this.share();
+          this.bindEvents();
+      },
 
-    function changeLinks (social, link) {
-      [].slice.call(social).forEach(function (el) {
-        return el.href = link;
-      });
-    }
+      bindEvents: function () {
+          document.addEventListener('DOMContentLoaded', this.contentLoaded);
+      },
 
-    changeLinks(twitter, twitterLink);
+      share: function () {
+          var twitter = document.querySelectorAll('.share--tw-js');
+          var position = document.location.href;
+          var title = document.title;
+          var twitterLink = 'https://twitter.com/intent/tweet?url=' + position + '&text=' + title + ' via @denoStc';
 
-  }
+          function changeLinks (social, link) {
+              [].slice.call(social).forEach(function (el) {
+                  return el.href = link;
+              });
+          }
 
-  share();
+          changeLinks(twitter, twitterLink);
 
-})();
+      },
+
+
+      contentLoaded: function () {
+          app.setTime('.element__sidebar--read-time', '.element__article--content', 'some-class');
+      },
+
+      setTime: function (target, wrapperEl, wrapperElClass) {
+          var wrapper = document.querySelector(wrapperEl).innerText,
+              targetEl = document.querySelector(target),
+              words = wrapper.trim().split(/\s+/g).length/180,
+              minutes = words.toFixed() < 1 ? 1 : words.toFixed(),
+              txt = minutes + ' min',
+              elem = document.createElement('span');
+
+          elem.className = wrapperElClass;
+          elem.innerText = txt;
+          targetEl.appendChild(elem);
+      }
+  };
+
+  app.init();
+
+})(window, document);
